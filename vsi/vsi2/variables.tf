79ab62458f3d1ea58ba10bbd6b17c8b83618cf86
JEAN-MICHEL OLIVA <jean-michel.oliva@fr.ibm.com>
2019-11-08 15:59:17 +0100

    modif

--- vsi/vsi2/variables.tf
+++ vsi/vsi2/variables.tf
@@ -1,11 +1,11 @@
 # Default Data Center for the creation of the Virtual Instance
 variable "vsi_datacenter" {
-  description = "Frankfurt"
-  default = "fra02"
+  description="Frankfurt"
+  default="fra02"
 }
 
 # Name of the Virtual Server Instance
 variable "vsi_instance_name" {
   description="Name of the instance"
-  default = "vsi-jmo-1"
+  default="vsi-jmo-1"
 }
